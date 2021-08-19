0774e58fbd76ecc96c216449437408b78f42f79e
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-19 08:14:48 +0600

    styling

--- src/Catalog.js
+++ src/Catalog.js
@@ -3,23 +3,27 @@ import dishes from "./dishes";
 import Rating from "./Rating";
 
 const Catalog = () => {
-  const [catalog, setCatalog] = useState(dishes)
-    return(
+    const [catalog, setCatalog] = useState(dishes)
+    return (
         <div className="row">
             {
                 catalog.map(el =>
                     <div className="col-4" key={el.id}>
                         <div className="dish__box">
-                            <img  src={el.image} className="dish__image" alt="dish" />
+                            <img src={el.image} className="dish__image" alt="dish"/>
                             <div className="dish__info-box">
                                 <div className="dish__info">
                                     <h2 className="dish__title">{el.title}</h2>
                                     <h2 className="dish__price">{el.price}</h2>
                                 </div>
                                 <p className="dish__desc">{el.desc}</p>
-                                <Rating rating={el.rating}/>
+                                <div className="dish__info">
+                                    <span className="dish__plus-sign">
+                                         <i className="fas fa-plus"/>
+                                    </span>
+                                    <Rating rating={el.rating}/>
+                                </div>
                             </div>
-
                         </div>
                     </div>
                 )
